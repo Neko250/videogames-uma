775d48e1a13042206de606134a1a7ec1c15d68e5
Neko250 <carlos.aguilar.112358@gmail.com>
2016-05-05 02:59:26 +0200

    fixed pong size

--- src/com/github/neko250/videogames/pong/Main.java
+++ src/com/github/neko250/videogames/pong/Main.java
@@ -4,8 +4,8 @@ import com.sun.j3d.utils.applet.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame(new View(), 1366, 768);
-        frame.setExtendedState(MainFrame.MAXIMIZED_BOTH);
+        MainFrame frame = new MainFrame(new View(), 512, 512);
+        // frame.setExtendedState(MainFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
     }
 }
